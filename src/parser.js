77f2d729dcfc059aafec5e902a271924435bf345
steshkof <steshkof@gmail.com>
2022-08-19 13:13:16 +0300

    check 1.1

--- src/parser.js
+++ src/parser.js
@@ -2,18 +2,26 @@ export default (rssContent) => {
   const parser = new DOMParser();
   const parsedRSS = parser.parseFromString(rssContent, 'application/xml');
 
-  const rssTitle = parsedRSS.querySelector('title').textContent;
-  const rssDescription = parsedRSS.querySelector('description').textContent;
+  const parserError = parsedRSS.querySelector('parsererror ');
 
-  const itemsArray = [...parsedRSS.querySelectorAll('item')];
-  const posts = itemsArray.map((item) => {
-    const itemTitle = item.querySelector('title').textContent;
-    const itemLink = item.querySelector('link').textContent;
-    const itemDescription = item.querySelector('description').textContent;
-    return { itemTitle, itemLink, itemDescription };
-  });
+  if (!parserError) {
+    const rssTitle = parsedRSS.querySelector('title').textContent;
+    const rssDescription = parsedRSS.querySelector('description').textContent;
 
-  return {
-    rssTitle, rssDescription, posts,
-  };
+    const itemsArray = [...parsedRSS.querySelectorAll('item')];
+    const posts = itemsArray.map((item) => {
+      const itemTitle = item.querySelector('title').textContent;
+      const itemLink = item.querySelector('link').textContent;
+      const itemDescription = item.querySelector('description').textContent;
+      return { itemTitle, itemLink, itemDescription };
+    });
+
+    return {
+      rssTitle, rssDescription, posts,
+    };
+  }
+
+  const errorContent = parserError.textContent;
+  console.error(errorContent);
+  throw new Error(parserError);
 };
